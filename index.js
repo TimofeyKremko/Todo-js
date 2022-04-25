d2d53d81320d88118fce54e62fa61d9564e31e55
Timofey <70627095+TimofeyKremko@users.noreply.github.com>
2022-04-25 12:41:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,170 @@
-<!DOCTYPE html>
-<html lang="en">
+let todoArray = [];
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            createTodoApp(document.getElementById('list-1'), 'Todo App', key = 'list-1');
-        });
-    </script>
-    <style>
-        li {
-            margin-top: 15px;
-        }
-        .btn-wrapper {
-            display: flex;
-            gap: 10px;
-        }
-        .nav__link {
-            display: inline-block;
-            margin-right: 20px;
-        }
-    </style>
-</head>
+const createAppTitle = (title) => {
+    const appTitle = document.createElement('h1');
+    appTitle.innerHTML = title;
 
-<body>
-    <div class="container">
-    </div>
-    <div id="list-1" class="container"></div>
-    <script src="index.js"></script>
-</body>
+    return appTitle;
+}
 
-</html>
+const createTodoForm = () => {
+    const form = document.createElement('form');
+    const input = document.createElement('input');
+    const addButton = document.createElement('button');
+    const wrapper = document.createElement('div');
+
+    addButton.disabled = !input.value.length;
+
+    input.addEventListener('input', () => {
+        addButton.disabled = !input.value.length;
+    });
+
+    form.classList.add('input-group', 'mb-3');
+    input.classList.add('form-control');
+    input.placeholder = 'Введите название дела';
+    addButton.classList.add('btn', 'btn-primary');
+    wrapper.classList.add('input-group-append');
+    addButton.textContent = 'Добавит дело';
+
+    wrapper.append(addButton);
+    form.append(input);
+    form.append(wrapper);
+
+    return {
+        form,
+        input,
+        addButton
+    }
+}
+
+const createTodoList = () => {
+    const list = document.createElement('ul');
+    list.classList.add('list-group');
+
+    return list;
+}
+
+const createTodoItem = (name) => {
+    const todoItem = document.createElement('li');
+    const btnWrapper = document.createElement('div');
+    const doneBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    const randomId = Math.random() * 15.75;
+    todoItem.id = randomId.toFixed(2);
+
+    todoItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    doneBtn.classList.add('btn', 'btn-success');
+    deleteBtn.classList.add('btn', 'btn-danger');
+    btnWrapper.classList.add('btn-wrapper')
+    todoItem.textContent = name;
+    doneBtn.textContent = 'Готово';
+    deleteBtn.textContent = 'Удалить';
+
+    btnWrapper.append(doneBtn, deleteBtn);
+    todoItem.append(btnWrapper);
+
+    return {
+        todoItem,
+        doneBtn,
+        deleteBtn,
+        btnWrapper
+    }
+}
+
+const changeItemDone = (arr, item) => {
+    arr.map(obj => {
+        if (obj.id === item.id & obj.done === false) {
+            obj.done = true;
+        } else if (obj.id === item.id & obj.done === true) {
+            obj.done = false;
+        }
+    });
+}
+
+const completeTodoItem = (item, btn) => {
+    btn.addEventListener('click', () => {
+        todoArray = JSON.parse(localStorage.getItem(key));
+        item.classList.toggle('list-group-item-success');
+        changeItemDone(todoArray, item);
+
+        localStorage.setItem(key, JSON.stringify(todoArray));
+    });
+}
+
+const deleteTodoItem = (item, btn) => {
+    btn.addEventListener('click', () => {
+            todoArray = JSON.parse(localStorage.getItem(key));
+
+            const neaList = todoArray.filter(obj => obj.id !== item.id);
+
+            localStorage.setItem(key, JSON.stringify(neaList));
+            item.remove();
+    });
+}
+
+function createTodoApp(container, title, key) {
+    const appTitle = createAppTitle(title);
+    const appForm = createTodoForm();
+    const appList = createTodoList();
+
+    container.append(appTitle, appForm.form, appList);
+
+    if (localStorage.getItem(key)) {
+        todoArray = JSON.parse(localStorage.getItem(key));
+
+        for (const obj of todoArray) {
+            const todoItem = createTodoItem(appForm.input.value);
+
+            todoItem.todoItem.textContent = obj.name;
+            todoItem.todoItem.id = obj.id;
+
+            if (obj.done == true) {
+                todoItem.todoItem.classList.add('list-group-item-success');
+            } else {
+                todoItem.todoItem.classList.remove('list-group-item-success');
+            }
+
+            completeTodoItem(todoItem.todoItem, todoItem.doneBtn);
+            deleteTodoItem(todoItem.todoItem, todoItem.deleteBtn);
+
+            appList.append(todoItem.todoItem);
+            todoItem.todoItem.append(todoItem.btnWrapper);
+        }
+    }
+
+    appForm.form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todoItem = createTodoItem(appForm.input.value);
+
+        if (!appForm.input.value) {
+            return;
+        }
+        completeTodoItem(todoItem.todoItem, todoItem.doneBtn);
+        deleteTodoItem(todoItem.todoItem, todoItem.deleteBtn);
+
+        let localStorageData = localStorage.getItem(key);
+
+        if (localStorageData == null) {
+            todoArray = [];
+        } else {
+            todoArray = JSON.parse(localStorageData);
+        }
+
+        const createItemObj = (arr) => {
+            const itemObj = {};
+            itemObj.name = appForm.input.value;
+            itemObj.id = todoItem.todoItem.id;
+            itemObj.done = false;
+
+            arr.push(itemObj);
+        }
+        createItemObj(todoArray);
+        localStorage.setItem(key, JSON.stringify(todoArray));
+
+        appList.append(todoItem.todoItem);
+        appForm.input.value = '';
+        appForm.addButton.disabled = !appForm.addButton.disabled;
+    });
+}
